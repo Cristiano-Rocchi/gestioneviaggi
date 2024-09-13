1e09d95e2b275cf9c6fa591bbccc6925de5ed16e
Cristiano-Rocchi <168540556+Cristiano-Rocchi@users.noreply.github.com>
2024-09-13 11:52:14 +0200

    Initial commit

---
+++ src/test/java/cristianorocchi/gestioneviaggi/GestioneviaggiApplicationTests.java
@@ -0,0 +1,13 @@
+package cristianorocchi.gestioneviaggi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioneviaggiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
